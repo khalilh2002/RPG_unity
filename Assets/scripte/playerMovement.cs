8e89dd8b1549197461fbd1f516c3912b5471baa5
El Mahdi Id Lahcen <idlahcen.elmahdi@etu.uae.ac.ma>
2024-05-02 02:04:41 +0100

    added animations

--- Assets/scripte/playerMovement.cs
+++ Assets/scripte/playerMovement.cs
@@ -4,18 +4,66 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    [SerializeField]float speed = 25f;
-   
-    // Update is called once per frame
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    [SerializeField] float speed = 25f;
+
+    bool IsRunning = false;
+    bool lastInputWasUp = false; // Default to up
+    bool lastInputWasDown = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate( new Vector3(horizontal,vertical , 0f) * speed *Time.deltaTime);
+        // Movement
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0f) * speed * Time.deltaTime);
 
+        // Check if the player is running
+        IsRunning = (horizontalInput != 0 || verticalInput != 0);
+        animator.SetBool("IsRunning", IsRunning);
+
+        // Flip sprite if moving horizontally
+        if (horizontalInput < 0)
+        {
+            spriteRenderer.flipX = true;
+            lastInputWasDown = false;
+            lastInputWasUp = false;
+        }
+        else if (horizontalInput > 0)
+        {
+            spriteRenderer.flipX = false;
+            lastInputWasDown = false;
+            lastInputWasUp = false;
+        }
+
+        // Set Idle animations
+        animator.SetBool("IsIdle", !IsRunning || (horizontalInput == 0 && verticalInput == 0 && !lastInputWasUp));
+        animator.SetBool("IsUp", lastInputWasUp && !IsRunning && horizontalInput == 0);
+        animator.SetBool("IsDown", lastInputWasDown && !IsRunning && horizontalInput == 0);
+
+        // Set RunningUp and RunningDown animations
+        animator.SetBool("IsRunningUp", verticalInput > 0 && IsRunning);
+        animator.SetBool("IsRunningDown", verticalInput < 0 && IsRunning);
+
+        // Update lastInputWasUp
+        if (verticalInput > 0)
+        {
+            lastInputWasUp = true;
+            lastInputWasDown = false;
+        }
+        else if (verticalInput < 0)
+        {
+            lastInputWasDown = true;
+            lastInputWasUp = false;
+            
+        }
     }
 }
-
-
-//hello test
