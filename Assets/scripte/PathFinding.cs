76a1b969ba3eb39469351483dfd0debc9e03b6a4
Khalil El Houssine <khalil.houssein2002@gmail.com>
2024-05-07 01:47:08 +0100

    path funding root left the movemtent

---
+++ Assets/scripte/PathFinding.cs
@@ -0,0 +1,237 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding
+{
+    private const int MOVE_STRAIGHT_COST = 10;
+    private const int MOVE_DIAGONAL_COST = 14;
+
+    private Grid<Node> gridPathFinding;
+    private  Vector3 origin = Vector3.zero;
+
+
+    private List<Node> openList;
+    private List<Node> closeList;
+
+
+    public PathFinding(int width , int height )
+    {
+        gridPathFinding = new Grid<Node>(width, height ,2f,origin, (Grid<Node> g, int x, int y) => new Node(g, x, y));
+        
+    }
+
+    public List<Node> FindPath(int startX , int startY , int endX , int endY)
+    {
+        Node startNode = gridPathFinding.GetGridObject(startX, startY);
+        Node endNode = gridPathFinding.GetGridObject(endX, endY);
+
+        openList = new List<Node> { startNode};
+        closeList = new List<Node>();
+
+        for (int x = 0; x < gridPathFinding.GetWidth(); x++)
+        {
+            for (int y = 0; y < gridPathFinding.GetHeight(); y++)
+            {
+                Node pathNode = gridPathFinding.GetGridObject(x, y);
+
+                if (pathNode==null)
+                {
+                    Debug.Log(" you path Node null 789 x = "+x+" y = "+y );
+                }
+                pathNode.gCost= int.MaxValue;
+                pathNode.CalculateFCost();
+                pathNode.cameFromNode = null;
+            }
+        }
+        startNode.gCost = 0;
+        startNode.hCost = CalculateDistanceCost(startNode ,endNode) ;
+        startNode.CalculateFCost();
+
+        while (openList.Count  > 0)
+        {
+            Node currentNode = GetLowestFCost(openList);
+            if (currentNode == endNode)
+            {
+                return CalculatePath(endNode);
+            }
+            openList.Remove(currentNode);
+            closeList.Add(currentNode);
+            foreach (Node node in GetNeighbourList(currentNode))
+            {
+                if ( closeList.Contains(node) )
+                {
+                    continue;
+                }
+                if (!node.isWalkable)
+                {
+                    closeList.Add(node);
+                    continue;
+                }
+                int tentativeGCost = currentNode.gCost + CalculateDistanceCost(currentNode , node);
+                if (tentativeGCost < node.gCost)
+                {
+                    node.cameFromNode = currentNode;
+                    node.gCost = tentativeGCost;
+                    node.hCost = CalculateDistanceCost(node, endNode);
+                    node.CalculateFCost();
+                    if (!openList.Contains(node))
+                    {
+                        openList.Add(node);
+                    }
+
+                }
+            }
+        }
+        // out of nodes in open list coldnt find the path 
+        return null;
+    }
+
+    private List<Node> GetNeighbourList(Node currentNode)
+    {
+        List<Node> neighbourList = new List<Node> ();
+        if (currentNode.X -1 >=0)
+        {
+            //left
+            neighbourList.Add(GetNode(currentNode.X - 1, currentNode.Y ));
+            if (currentNode.Y - 1 >= 0)
+            {
+                //down left
+                neighbourList.Add(GetNode(currentNode.X - 1, currentNode.Y-1));
+
+            }
+            if (currentNode.Y + 1 < gridPathFinding.GetHeight())
+            {
+                //down up
+                neighbourList.Add(GetNode(currentNode.X - 1, currentNode.Y +1));
+
+            }
+
+        }
+        if (currentNode.X + 1 < gridPathFinding.GetWidth())
+        {
+            //right
+            neighbourList.Add(GetNode(currentNode.X + 1, currentNode.Y));
+            if (currentNode.Y - 1 >= 0)
+            {
+                //down left
+                neighbourList.Add(GetNode(currentNode.X + 1, currentNode.Y - 1));
+
+            }
+            if (currentNode.Y + 1 < gridPathFinding.GetHeight())
+            {
+                //down up
+                neighbourList.Add(GetNode(currentNode.X + 1, currentNode.Y + 1));
+
+            }
+
+        }
+
+        if (currentNode.Y + 1 < gridPathFinding.GetHeight())
+        {
+            //down up
+            neighbourList.Add(GetNode(currentNode.X , currentNode.Y + 1));
+
+        }
+        if (currentNode.Y - 1  >= 0)
+        {
+            //down up
+            neighbourList.Add(GetNode(currentNode.X, currentNode.Y - 1));
+
+        }
+        return neighbourList;
+
+    }
+
+
+
+    
+
+    private List<Node> CalculatePath(Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        path.Add(endNode);
+        Node currentNode = endNode;
+        while (currentNode.cameFromNode != null)
+        {
+            path.Add(currentNode.cameFromNode);
+            currentNode = currentNode.cameFromNode;
+        }
+        path.Reverse();
+        return path;
+    }
+    public Node GetNode(int x, int y)
+    {
+        return gridPathFinding.GetGridObject(x, y);
+    }
+    public Grid<Node> GetGrid()
+    {
+        return gridPathFinding;
+    }
+
+    private Node GetLowestFCost(List<Node> pathNodeList)
+    {
+        Node lowestFCost = pathNodeList[0];
+        for (int i = 0; i < pathNodeList.Count; i++)
+        {
+            if (pathNodeList[i].fCost < lowestFCost.fCost )
+            {
+                lowestFCost = pathNodeList[i];
+            }
+        }
+        return lowestFCost;
+    }
+
+
+    private int CalculateDistanceCost(Node a , Node b)
+    {
+        int xDistance = Mathf.Abs(a.X - b.X);
+        int yDistance = Mathf.Abs(a.Y - b.Y);
+
+        int remaining = Mathf.Abs(xDistance-yDistance) ;
+
+        int res = MOVE_DIAGONAL_COST * Mathf.Min(xDistance, yDistance) * MOVE_STRAIGHT_COST * remaining;
+        return res;
+    }
+
+
+    public void createGrid(HashSet<Vector2Int> floor)
+    {
+        gridPathFinding.createGrid(floor);
+    }
+
+
+    public void createWalkabelGrid(HashSet<Vector2Int> floor)
+    {
+        for (int x = 0; x < gridPathFinding.GetWidth(); x++)
+        {
+            for (int y = 0; y < gridPathFinding.GetHeight(); y++)
+            {
+                Vector2Int xitem = new Vector2Int((int)gridPathFinding.getWorldPosition(x, y).x, (int)gridPathFinding.getWorldPosition(x, y).y);
+                if (floor == null)
+                {
+                    Debug.Log("value is null 1254");
+                }
+                else if (floor.Contains(xitem))
+                {
+                    //CreateWorldText(gridArray[x, y].ToString(), null, getWorldPosition(x, y), 20, Color.green, TextAnchor.MiddleCenter).tag = "gridCell";
+                    //Debug.DrawLine(getWorldPosition(x, y), getWorldPosition(x + 1, y), UnityEngine.Color.white, 100f);
+                    //Debug.DrawLine(getWorldPosition(x, y), getWorldPosition(x, y + 1), UnityEngine.Color.white, 100f);
+                    GetNode(x, y).setIsWalkable(true);
+                }
+                else
+                {
+                    //CreateWorldText(gridArray[x, y].ToString(), null, getWorldPosition(x, y), 20, Color.red, TextAnchor.MiddleCenter).tag = "gridCell";
+                    //Debug.DrawLine(getWorldPosition(x, y), getWorldPosition(x + 1, y), UnityEngine.Color.blue, 100f);
+                    //Debug.DrawLine(getWorldPosition(x, y), getWorldPosition(x, y + 1), UnityEngine.Color.blue, 100f);
+
+                    GetNode(x, y).setIsWalkable(false);
+
+                }
+
+            }
+        }
+    }
+
+}
--- Assets/scripte/test.cs
+++ Assets/scripte/test.cs
@@ -1,49 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class test : MonoBehaviour
 {
     [SerializeField] RoomFirstMapGenerator roomFirstMapGenerator;
+    [SerializeField] Transform player;
 
-    Grid g;
-    // Start is called before the first frame update
-    void Start()
+    private PathFinding pFinding;
+
+    private void Start()
     {
-        int x = roomFirstMapGenerator.getMapWidth;
-        int y = roomFirstMapGenerator.getMapHeight;
-        float cell = 2f;
+        this.pFinding = new PathFinding(roomFirstMapGenerator.getMapWidth, roomFirstMapGenerator.getMapHeight );
+        this.pFinding.createWalkabelGrid(roomFirstMapGenerator.getFloor);
+        this.pFinding.createGrid(roomFirstMapGenerator.getFloor);
 
-        g = new Grid((int)(x / cell) + 1, (int)(y / cell) + 1, cell , roomFirstMapGenerator.getFloor , Vector3.zero);
-        
+
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (pFinding == null)
+            {
+                Debug.Log(" in K pFindfin is null 789 ");
+
+            }
+
+
+            Vector3 mousDownPosition = GetMouseWorldPosition();
+            pFinding.GetGrid().GetXY(mousDownPosition, out int x, out int y);
+            
+            Debug.Log("mouse is wlakbale 789"+ pFinding.GetNode(x, y).isWalkable.ToString());
+            
+
+
+            pFinding.GetGrid().GetXY(player.position, out int x1, out int y1);
+            
+                Debug.Log("playe is wlakbale 789 " + pFinding.GetNode(x1, y1).isWalkable.ToString());   
+            
+
+            List<Node> path = pFinding.FindPath(x1, y1, x, y);
+            if (path != null)
+            {
+                for (int i = 0; i < path.Count-1; i++)
+                {
+                    Debug.DrawLine(new Vector3(path[i].X, path[i].Y) * 2f + Vector3.one * 1f, new Vector3(path[i + 1].X, path[i + 1].Y) * 2f + Vector3.one * 1f, Color.red,100f);
+                }
+            }
+            else
+            {
+                Debug.Log("path is null 789 " );
+
+            }
+
+        }
         if (Input.GetKey(KeyCode.G))
         {
-            removeAll();
-            int x = roomFirstMapGenerator.getMapWidth;
-            int y = roomFirstMapGenerator.getMapHeight;
-            float cell = 2f;
+            //pFinding = new PathFinding(roomFirstMapGenerator.getMapWidth, roomFirstMapGenerator.getMapHeight);
 
-            g = new Grid((int)(x / cell) + 1, (int)(y / cell) + 1, cell, roomFirstMapGenerator.getFloor , Vector3.zero);
+            if (roomFirstMapGenerator.getFloor == null)
+            {
+                Debug.Log(" floor is null 789 ");
+            }
+            else if (pFinding == null) {
+                Debug.Log(" pFindfin is null 789 ");
+
+            }
+            else
+            {
+                this.pFinding.createWalkabelGrid(roomFirstMapGenerator.getFloor);
+                pFinding.createGrid(roomFirstMapGenerator.getFloor);
+
+            }
 
         }
 
+       
+
+
     }
 
-    void removeAll()
+    /////////////////////////////
+    // Get Mouse Position in World with Z = 0f
+    public static Vector3 GetMouseWorldPosition()
     {
-        // Find all existing coins and enemies
-        GameObject[] existingCoins = GameObject.FindGameObjectsWithTag("gridCell");
-
-        // Destroy all existing coins and enemies except the original ones
-        foreach (GameObject coin in existingCoins)
-        {
-                Destroy(coin);
-        }
-
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
     }
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+    /////////////////////////////////
 }
